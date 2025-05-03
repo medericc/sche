eb335ee08474199bd48dad2a2a68b566312972c5
medericc <bleublanctour1@>
2025-05-03 07:25:55 +0200

    ee

--- app/page.tsx
+++ app/page.tsx
@@ -182,7 +182,7 @@ export default function ValkyriesSchedulePage() {
                   </div>
                 </div>
               </CardContent>
-              <CardFooter className="bg-purple-800 p-2 rounded-b-xl flex justify-center">
+              <CardFooter className="bg-purple-700 p-2 rounded-b-xl flex justify-center">
     <a
       href={match.link} // Assure-toi que match.link contient une URL valide
       target="_blank"
